1234a1db9343de65fc4243b25bd6a29536d842c5
[AI7/2반]이승빈 <sngbn95@gmail.com>
2023-06-09 18:59:33 +0900

    feat-admin 화면 기본 구조, msw 설치완료-[이승빈]0609 19:00

---
+++ src/mocks/handlers.js
@@ -0,0 +1,32 @@
+import { rest } from 'msw'
+
+export const handlers = [
+    // Handles a POST /login request
+    rest.post('/report/register', (req, res, ctx)=>{
+        console.log(req.body)
+        return res(
+            ctx.status(201),
+        )
+    }),
+  
+    // Handles a GET /user request
+    rest.get('/admin/report', (req, res, ctx)=>{
+        return res(
+            ctx.status(200),
+            ctx.json([
+                {
+                    userId : 'elice1',
+                    attackerId : '갑수',
+                    content : '멍청이',
+                    violenceAt : '20020306'
+                },
+                {
+                    userId : 'elice2',
+                    attackerId : '종수',
+                    content : '바보',
+                    violenceAt : '20020305'
+                }
+            ])
+        )
+    }),
+  ]
