78f04fea74b8cc0f8e7aa9cab3fe2abe5679a553
YooSeungJe <dbtmdwp3562@gmail.com>
2023-06-10 22:53:00 +0900

    충돌해결

--- src/api.js
+++ src/api.js
@@ -5,11 +5,9 @@ const instance = axios.create({
   baseURL: 
   // process.env.NODE_ENV === 'development' ? '/' : 
   'http://localhost:3001'
-  // process.env.NODE_ENV === 'development' ? '/' : 
-  'http://localhost:3001'
 });
 
-const postFormData = async (endpoint, data) => {
+
 const postFormData = async (endpoint, data) => {
   const formData = new FormData();
   for (const key in data) {
@@ -19,7 +17,6 @@ const postFormData = async (endpoint, data) => {
   try {
     const response = await instance.post(endpoint, formData, {
       headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
         Authorization: `Bearer ${localStorage.getItem("token")}`,
         "Content-Type": "multipart/form-data"
       },
@@ -48,22 +45,7 @@ const getFormData = async (endpoint, formData) => {
  
 }
 
-const getFormData = async (endpoint, formData) => {
-  try {
-    const response = await instance.get(endpoint, formData,
-      {
-        headers: {
-          Authorization: `Bearer ${sessionStorage.getItem("userToken")}`,
-        },
-        responseType: "blob", // blob 데이터로 받기 위해 responseType 설정
-      }
-    );
-    return response
-  } catch (error) {
-    console.error(error);
-  }
- 
-}
+
 
 const get = async (url, params) => {
   const token = localStorage.getItem('token');
