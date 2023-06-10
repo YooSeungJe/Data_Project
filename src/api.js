b9303312df1e7b330d9e0558d409c39c86ebb033
YooSeungJe <dbtmdwp3562@gmail.com>
2023-06-10 22:45:15 +0900

    충돌해결

--- src/api.js
+++ src/api.js
@@ -3,10 +3,11 @@ import axios from 'axios';
 // axios instance 생성
 const instance = axios.create({
   baseURL: 
+  // process.env.NODE_ENV === 'development' ? '/' : 
   'http://localhost:3001'
 });
 
-async function post(endpoint, data) {
+const postFormData = async (endpoint, data) => {
   const formData = new FormData();
   for (const key in data) {
     formData.append(key, data[key]);
@@ -15,9 +16,27 @@ async function post(endpoint, data) {
   try {
     const response = await instance.post(endpoint, formData, {
       headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
         "Content-Type": "multipart/form-data"
       },
     });
+    return response
+  } catch (error) {
+    console.error(error);
+  }
+ 
+}
+
+const getFormData = async (endpoint, formData) => {
+  try {
+    const response = await instance.get(endpoint, formData,
+      {
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("userToken")}`,
+        },
+        responseType: "blob", // blob 데이터로 받기 위해 responseType 설정
+      }
+    );
     return response
   } catch (error) {
     console.error(error);
@@ -40,6 +59,4 @@ const get = async (url, params) => {
   }
 }
 
-
-
-export {post, get};
+export {postFormData, getFormData, get};
