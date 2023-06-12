6915fa983cfaabbb5fcdbee604d84966339c641e
유희조 <dbgmlwh@gmail.com>
2023-06-12 13:14:45 +0900

    feat-통계추가-[유희조]_0612_13:14

--- src/pages/Statics.js
+++ src/pages/Statics.js
@@ -4,14 +4,45 @@ import { Bar, Pie } from 'react-chartjs-2';
 import Chart from 'chart.js/auto';
 
 const Statistics = () => {
-  const [reportTierRatioData, setReportTierRatioData] = useState([]);
-  const [reportCntByMonthData, setReportCntByMonthData] = useState([]);
+  const [genderData, setGenderData] = useState([]); // 성별
+  const [reportTierRatioData, setReportTierRatioData] = useState([]); // 티어별 신고 횟수
+  const [abuseCntByCategoryData, setAbuseCntByCategoryData] = useState([]); // 신고된 카테고리 누적횟수
+  const [userCntByMannerGradeData, setUserCntByMannerGradeData] = useState([]); // manner_grade 별 누적
+  const [reportCntByMonthData, setReportCntByMonthData] = useState([]); // 월별 신고 누적 횟수
 
   useEffect(() => {
     fetchReportTierRatioData();
     fetchReportCntByMonth();
+    fetchAbuseCntByCategory();
+    fetchUserCntByMannerGrade();
+    fetchGenderData();
   }, []);
 
+  // 성별
+  const fetchGenderData = async () => {
+    try {
+      const response = await axios.get(
+        'http://localhost:3001/stats/genderRatio'
+      );
+      const genderRatioData = response.data;
+      setGenderData(genderRatioData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const genderRatio = {
+    labels: ['Male', 'Female'],
+    datasets: [
+      {
+        data: [genderData, 100 - genderData],
+        backgroundColor: ['#FF6384', '#36A2EB'],
+        hoverBackgroundColor: ['#FF6384', '#36A2EB'],
+      },
+    ],
+  };
+
+  // 티어별 신고 횟수
   const fetchReportTierRatioData = async () => {
     try {
       const response = await axios.get(
@@ -19,18 +50,6 @@ const Statistics = () => {
       );
       const reportTierRatio = response.data;
       setReportTierRatioData(reportTierRatio);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const fetchReportCntByMonth = async () => {
-    try {
-      const response = await axios.get(
-        'http://localhost:3001/stats/reportCntByMonth'
-      );
-      const reportCntByMonth = response.data;
-      setReportCntByMonthData(reportCntByMonth);
     } catch (error) {
       console.error(error);
     }
@@ -47,6 +66,68 @@ const Statistics = () => {
     ],
   };
 
+  // 카테고리별 누적 횟수
+  const fetchAbuseCntByCategory = async () => {
+    try {
+      const response = await axios.get(
+        'http://localhost:3001/stats/abuseCntByCategory'
+      );
+      const abuseCntByCategory = response.data;
+      setAbuseCntByCategoryData(abuseCntByCategory);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const abuseCntByCategory = {
+    labels: abuseCntByCategoryData.map(item => item.category_name),
+    datasets: [
+      {
+        label: '신고 횟수',
+        data: abuseCntByCategoryData.map(item => item.count),
+        backgroundColor: 'rgba(75,192,192,1)',
+      },
+    ],
+  };
+
+  // manner_grade별 누적 횟수
+  const fetchUserCntByMannerGrade = async () => {
+    try {
+      const response = await axios.get(
+        'http://localhost:3001/stats/loluserCntByMannerGrade'
+      );
+      const loluserCntByMannerGrade = response.data;
+      setUserCntByMannerGradeData(loluserCntByMannerGrade);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const loluserCntByMannerGrade = {
+    labels: userCntByMannerGradeData.map(item => item.manner_grade),
+    datasets: [
+      {
+        label: '유저 수',
+        data: userCntByMannerGradeData.map(item => item.grade_count),
+        backgroundColor: 'rgba(75,192,192,1)',
+      },
+    ],
+  };
+
+  // 월별 신고 누적 횟수
+  const fetchReportCntByMonth = async () => {
+    try {
+      const response = await axios.get(
+        'http://localhost:3001/stats/reportCntByMonth'
+      );
+      const reportCntByMonth = response.data;
+      setReportCntByMonthData(reportCntByMonth);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // 월별 신고 누적 횟수
   const reportCntByMonth = {
     labels: reportCntByMonthData.map(item => item.month),
     datasets: [
@@ -85,7 +166,15 @@ const Statistics = () => {
   return (
     <div>
       <h1>통계 페이지</h1>
+      성별
+      <Pie data={genderRatio} />
+      티어별 신고 횟수
       <Bar data={reportTierRatio} />
+      카테고리별 신고 횟수
+      <Bar data={abuseCntByCategory} />
+      manner_grade 별 신고 횟수
+      <Bar data={loluserCntByMannerGrade} />
+      월별 신고 횟수
       <Pie data={reportCntByMonth} />
     </div>
   );
