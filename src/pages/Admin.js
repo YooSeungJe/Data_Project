82ef48bf73fdb6254a66ef87da9c10ca2a85f692
[AI7/2반]이승빈 <sngbn95@gmail.com>
2023-06-14 16:29:25 +0900

    feat-admin 구현완료-[이승빈]0614 1630

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,24 +1,29 @@
 import {useState, useEffect, useCallback} from 'react';
-import { Card, Button, Container, Row, Col, Dropdown, ButtonGroup, ToggleButton } from 'react-bootstrap';
 import './css/Admin.css';
+import buttonStyles from './css/Button.module.css';
+import dropdownStyles from './css/DropDown.module.css'
 import * as Api from '../api.js';
+import Header from './Header.js';
 
 export default function Admin () {
     const [asserts, setAsserts] = useState([]);
     const [selectedItem, setSelectedItem] = useState({});
     const [reportId, setReportId] = useState('');
     const [blob, SetBlob] = useState(null);
+
+    // assert-card 테두리 유무
+    const [isCardClicked, setIsCardClicked] = useState('');
+    
     // 페이지네이션
     const [currentPage, setCurrentPage] = useState(0);
     const [totalPage, setTotalPage] = useState(1);
-    const [sort, Setsort] = useState('old');
+    const [sort, SetSort] = useState('old');
     const [statusPage, SetStatusPage] = useState('pending');
-
-    const categoryList = ['clean', '기타 혐오', '남성', '성소수자', '악플/욕설', '여성/가족', '연령', '인종/국적', '종교', '지역'];
 
     const fetchData = useCallback(async () => {
       const result = await Api.get(`/admin/report?sort=${sort}&status=${statusPage}&currentPage=${currentPage}`);
       setAsserts(result.data);
+      console.log(result.data);
       setTotalPage(result.totalPages);
     },[sort, statusPage, currentPage]);
 
@@ -35,12 +40,14 @@ export default function Admin () {
         return result;
       }
     }, [reportId]);
-
+  
+    // 상세 보기
     const handleItemClick = async (assert) => {
       setReportId(assert.id);
       const reportList = await getReportList();
       const addedAssert = {...assert, 'reportLists': reportList};
       setSelectedItem(addedAssert);
+      setIsCardClicked(assert.id); // assert-card 테두리 변수
     };
 
   // category name 변경 함수
@@ -55,24 +62,40 @@ export default function Admin () {
       setSelectedItem(addedAssert);
     };
 
-    // reportList 의 report에 접근하기 위해서 dropdown 을 그냥 하나의 component로 빼와서 만들어버림
-    const ReportDropdown = ({report, handleCategorySelect}) => {
-      const handleSelect = (selectedCategory) => {
-        handleCategorySelect(selectedCategory, report.content);
-      }
-      return (
-        <Dropdown onSelect={handleSelect}>
-          <Dropdown.Toggle variant="success" id="dropdown-basic">
-            {report.category_name}
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            {categoryList.map((category, index) => (
-              <Dropdown.Item eventKey={category} key={index}>{category}</Dropdown.Item>
+  // reportList 의 report에 접근하기 위해서 dropdown 을 그냥 하나의 component로 빼와서 만들어버림
+  const ReportDropdown = ({ report, handleCategorySelect}) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const categoryList = ['clean', '기타 혐오', '남성', '성소수자', '악플/욕설', '여성/가족', '연령', '인종/국적', '종교', '지역'];
+
+    const handleSelect = (selectedCategory) => {
+      handleCategorySelect(selectedCategory, report.content);
+      setIsOpen(false);
+    };
+
+    const toggleOpen = () => setIsOpen(!isOpen);
+
+    return (
+      <div className={dropdownStyles.dropdown}>
+        <button className={dropdownStyles.dropdown} onClick={toggleOpen}>
+          {report.category_name} &#x25BC;
+        </button>
+        {isOpen && (
+          <div className={dropdownStyles.dropdownMenu}>
+          {categoryList.map((category, index) => (
+              <button
+                className={dropdownStyles.dropdownItem}
+                onClick={() => handleSelect(category)}
+                key={index}
+              >
+                {category}
+              </button>
             ))}
-          </Dropdown.Menu>
-       </Dropdown>
-      );
-    }
+          </div>
+        )}
+      </div>
+    );
+  };
+
     // 신고 사진
     const ImageFromBlob = ({blob}) => {
       const [imageUrl, setImageUrl] = useState(null);
@@ -85,16 +108,17 @@ export default function Admin () {
       if (!imageUrl) {
         return null;
     }
-    return <img src={imageUrl} alt="From Blob" style={{display: 'block'}}/>;
+    return <img src={imageUrl} alt="From Blob" style={{display: 'block', width:' 100%', borderRadius:'0.75rem'}}/>;
     }
+
     // 확인 버튼 누르면 신고의 status 변경
-    const handleStatus = async (report) => {
+    const handleStatus = async (report, statusString) => {
       const result = await Api.patch('/admin/status',{
         reportId : reportId,
-        status : 'completed',
+        status : statusString,
       })
       console.log(result);
-      alert('해당 신고는 승인되었습니다')
+      alert(`해당 신고는 ${statusString=='completed'?'승인':'거절'}되었습니다`)
       setSelectedItem({});
       SetBlob(null);
     }
@@ -106,75 +130,77 @@ export default function Admin () {
     }
 
     // 체크 박스
-    const radios = [
-      { name: 'pending', value: 'pending' },
-      { name: 'completed', value: 'completed' },
-      { name: 'rejected', value: 'rejected' },
-      { name: 'all', value: 'all' },
-    ];
-
-    useEffect(()=>{console.log(statusPage)},[statusPage]);
+    const statusList = ['pending', 'completed', 'rejected', 'all'];
+    const sortList = ['old', 'new'];
 
       return (
-        <Container fluid>
-      <Row>
-        <Col sm={4}>
-          <ButtonGroup>
-            {radios.map((radio, idx) => (
-            <ToggleButton
-              key={idx}
-              id={`radio-${idx}`}
-              type="radio"
-              variant={'outline-success'}
-              name="radio"
-              value={radio.value}
-              checked={SetStatusPage === radio.value}
-              onChange={(e) => SetStatusPage(e.currentTarget.value)}
-            >
-            {radio.name}
-            </ToggleButton>
+        <>
+        <Header/>
+        <div className='admin-container'>
+        <div className='assert-container list'>
+        <div className='radio-container'>
+          <div className="status-container">
+            {statusList.map((stat, index) => (
+            <div key={index}>
+              <input type="radio" id={`radio${index}`} name="radioGroup1" value={stat} onClick={()=>{SetStatusPage(stat)}}/>
+              <label for={`radio${index}`}>{stat}</label>
+            </div>
+            ))}
+          </div>
+          <div className="sort-container">
+            {sortList.map((sort, index) => (
+            <div key={index}>
+              <input type="radio" id={`radio${index}`} name="radioGroup2" value={sort} onClick={()=>{SetSort(sort)}}/>
+              <label for={`radio${index}`}>{sort}</label>
+            </div>
+            ))}
+          </div>
+        </div>
+          {asserts.map((assert, index) => (
+            <div key={index} className='assert-card' style={{border: assert.id == isCardClicked ? '2px solid black' : 'none'}}>
+              <div key={index*100} className='assert-card-content'>
+                <h2>신고한 분: {assert.user_id}</h2>
+                <h3>욕한 놈: {assert.attacker_id}</h3>
+              </div>
+              <button className="status-button">{assert.status.charAt(0)}</button>
+              <button className={buttonStyles.button} onClick={() => handleItemClick(assert)}>
+                <span className={buttonStyles.button_top}>상세 보기</span>
+              </button>
+            </div>
           ))}
-      </ButtonGroup>
-          {asserts.map((assert) => (
-            <Card key={assert.Id} style={{ marginBottom: '10px' }}>
-              <Card.Body>
-                <Card.Title>{assert.user_id}</Card.Title>
-                <Card.Subtitle>{assert.attacker_id}</Card.Subtitle>
-                <Button variant="primary" onClick={() => handleItemClick(assert)}>
-                  상세 보기
-                </Button>
-              </Card.Body>
-            </Card>
-          ))}
-          <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 0}>이전</button>
-          {pageNumbers.map(number => (<button key={number} onClick={() => setCurrentPage(number)} style={{ backgroundColor: number === currentPage ? 'blue' : 'white' }}>{number}</button>))}
-          <button onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === totalPage}>다음</button>
-        </Col>
-        <Col sm={8}>
+            <div className='page-number'>
+              <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 0}>이전</button>
+              {pageNumbers.map(number => (<button key={number} onClick={() => setCurrentPage(number)} style={{ backgroundColor: number === currentPage ? 'blue' : 'white' }}>{number}</button>))}
+              <button onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === totalPage}>다음</button>
+            </div>
+        </div>
+        <div className='assert-container content'>
           {selectedItem && (
-            <div>
-              <h2>{selectedItem.attacker_id}</h2>
-              <p>{selectedItem.content}</p>
-              <p>{selectedItem.violence_at}</p>
+            <div className='assert-detail-card'>
+              <ImageFromBlob blob={blob}/>
+              <h2>욕한 놈: {selectedItem.attacker_id}</h2>
+              <p>욕 먹은 날짜 :{selectedItem.violence_at}</p>
+              <p>신고한 날짜: {selectedItem.created_at}</p>
+              <p className='situation'>상황 설명: {selectedItem.content}</p>
               {selectedItem.reportLists && (selectedItem.reportLists.map((report, index)=>{
                 return (
-                  <div key={index}>
+                  <div key={index*1000} className='assert-detail-content'>
                     {report.content}
                     <ReportDropdown report={report} handleCategorySelect={handleCategorySelect}/>
                   </div>
                 )
               }))}
-              <ImageFromBlob blob={blob}/>
             {Object.keys(selectedItem).length !== 0 && (
-              <>
-                <Button variant="danger" onClick={()=>{handleStatus(selectedItem); fetchData();}}>확인</Button>
-                <Button variant="warning" onClick={()=>{setSelectedItem({}); SetBlob(null);}}>취소</Button>
-              </>
+              <div className='okay-container'>
+                <button className='okay completed' onClick={()=>{handleStatus(selectedItem, 'completed'); fetchData();}}>승인</button>
+                <button className='okay completed' onClick={()=>{handleStatus(selectedItem, 'rejected'); fetchData();}}>반려</button>
+                <button className='okay rejected' onClick={()=>{setSelectedItem({}); SetBlob(null);}}>취소</button>
+              </div>
             )}
             </div>
           )}
-        </Col>
-      </Row>
-    </Container>
+        </div>
+      </div>
+      </>
   );
 };
